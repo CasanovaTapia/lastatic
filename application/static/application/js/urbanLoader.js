0badae62e6f0d231f9530c49863bd36776bd3b48
Dave Chason <chason@lapel.co>
2015-02-24 14:29:46 -0800

    Updatez

--- application/static/application/js/urbanLoader.js
+++
@@ -1,7 +0,0 @@
-$( '#urban' ).click( function( e ) {
-  e.preventDefault();
-  $.swipebox( [
-    { src:'{{STATIC_URL}}application/img/urban/pic1.jpg'},
-    { src:'{{STATIC_URL}}application/img/urban/pic2.jpg'}
-  ] );
-} );
